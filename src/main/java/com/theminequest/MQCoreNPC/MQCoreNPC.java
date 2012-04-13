95597e10bb7160019646c177a5a153b6cc7f149b
Alan Campbell <echobob@theminequest.com>
2012-04-13 14:29:47 -0400

    Added a method to make a new config file from a default in the jar.
    It's messy >.> I know.

--- src/main/java/com/theminequest/MQCoreNPC/MQCoreNPC.java
+++ src/main/java/com/theminequest/MQCoreNPC/MQCoreNPC.java
@@ -1,13 +1,37 @@
 package com.theminequest.MQCoreNPC;
 
+import java.io.File;
+import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 
+import com.theminequest.MineQuest.MineQuest;
+
 public class MQCoreNPC extends JavaPlugin {
 	private static PluginDescriptionFile description = null;
+	
+	//Quest Giver npcs
+	public FileConfiguration npcQuestGivers = null;
+	public File questGivers = null;
+	public String questGiversFileName = "Quest_Givers.yml";
+	//Mercenaries npcs
+	public FileConfiguration npcMercs = null;
+	public File mercs = null;
+	public String mercsFileName = "Mercenary_Npcs.yml";
+	//Merchant npcs
+	public FileConfiguration npcMerchant = null;
+	public File merchant = null;
+	public String merchantFileName = "Merchant_Npcs.yml";
+	//General npcs
+	public FileConfiguration npcGeneral = null;
+	public File general = null;
+	public String gerenalFileName = "General_Npcs.yml";
+	
 	
 	public static void log(String msg) {
 		log(Level.INFO, msg);
@@ -25,8 +49,25 @@ public class MQCoreNPC extends JavaPlugin {
 		return description.getName();
 	}
 	
+	public File loadNewConfig(String fileName, File storage) {
+	    File npcPropFile = new File(MineQuest.activePlugin.getDataFolder().getAbsolutePath()+File.separator+"npcs", fileName);
+	    FileConfiguration defaults = YamlConfiguration.loadConfiguration(storage);
+	 
+	    // Look for defaults in the jar
+	    InputStream defConfigStream = getResource(fileName+".yml");
+	    if (defConfigStream != null) {
+	        YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+	        defaults.setDefaults(defConfig);
+	    }
+	    return npcPropFile;
+	}
+	
 	@Override
 	public void onEnable() {
+		if (!getDataFolder().exists())
+			getDataFolder().mkdirs();
+		description = this.getDescription();
+		
 		//TODO: Load Command front end.
 		//TODO: Load Permanent npcs.
 		//TODO: Load properties for npcs.
--- src/main/java/com/theminequest/MQCoreNPC/Storage/NpcYaml.java
+++
@@ -1,5 +0,0 @@
-package com.theminequest.MQCoreNPC.Storage;
-
-public class NpcYaml {
-	
-}
