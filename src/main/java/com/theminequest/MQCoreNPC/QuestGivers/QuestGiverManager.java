5276bb61893cd4b86869339df4f303e452819087
Alan Campbell <echobob@theminequest.com>
2012-05-06 14:42:45 -0400

    Starting to get somewhere.
    Basic spawn code and file creation.

--- src/main/java/com/theminequest/MQCoreNPC/QuestGivers/QuestGiverManager.java
+++ src/main/java/com/theminequest/MQCoreNPC/QuestGivers/QuestGiverManager.java
@@ -3,6 +3,8 @@ package com.theminequest.MQCoreNPC.QuestGivers;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -11,25 +13,31 @@ import java.util.Map.Entry;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.ini4j.Ini;
 import org.ini4j.InvalidFileFormatException;
 import org.ini4j.Profile.Section;
 import org.ini4j.spi.IniBuilder;
 
 import com.theminequest.MQCoreNPC.MQCoreNPC;
+import com.topcat.npclib.NPCManager;
+import com.topcat.npclib.entity.NPC;
 
 public class QuestGiverManager {
-	
 	private File file;
 	private String name;
-	private long id;
-	private Location location;
-	private String skin;
-	private String cape;
+	private Location location;                                                         
+	private int locX;                                                                  
+	private int locY;                                                                  
+	private int locZ;                                                                  
+	private String skin;                                                               
+	private String cape;                                                               
 	private String noquestmessage;
 	private String havequestmessage;
 	private String[] quests;
-	private long[] recommend;
+	private String[] recommend;
+	private QuestGiverStorage storage = new QuestGiverStorage();
+	public NPCManager QuestGiverManager = new NPCManager(MQCoreNPC.activePlugin);
 	
 	/**
 	 * Creates a default file for the given npc name and location. 
@@ -38,37 +46,84 @@ public class QuestGiverManager {
 	 * @throws IOException
 	 */
 	public void CreateQuestNPC(String n, Location l) throws IOException{
-		name = n;
-		id = 0; // Implement some sort of tracking mechanism in NPCManager
+		name = n.replaceAll(" ", "_");;
 		location = l;
 		skin = "http://www.minecraft.net/images/char.png";
 		cape = "";
 		noquestmessage = "Sorry, I don't have anything for ya.";
 		havequestmessage = "Let's get started.";
 		quests = new String[0];
-		recommend = new long[0];
+		recommend = new String[0];
 		file = new File(MQCoreNPC.activePlugin.getDataFolder()+"NPC/"+"General/"+name+".npc");
 		file.createNewFile();
-	//	save();
+		storage.save(name, location, skin, cape, noquestmessage, havequestmessage, quests, recommend);
+		spawnNPC(location.getWorld(), location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
 	}
 	
+	public List<NPC> npcs() {
+		return QuestGiverManager.getNPCs();
+	}
+	
+	public void spawnNPC(World world, double x, double y, double z, float yaw, float pitch) {
+		Location spawnLocation = new Location(world, x, y, z, yaw, pitch);
+		QuestGiverManager.spawnHumanNPC(name, spawnLocation);
+	}
+	
+	public void spawnExistingNPCs(){
+		File f = new File(MQCoreNPC.activePlugin.getDataFolder()+"NPC/"+"General/");
+		ArrayList<File> files = new ArrayList<File>(Arrays.asList(f.listFiles()));
+		int n = 0;
+		while (n <= files.size()){
+			
+			try {
+				storage.LoadNPCDescription(files.get(n));
+				name = storage.getName();
+				location = storage.getLocation();
+				spawnNPC(location.getWorld(), location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());				
+			} catch (InvalidFileFormatException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}	
+			n++;
+		}
+	}
+	
+	/**
+	 * 
+	 * @return
+	 * @throws IOException 
+	 * @throws InvalidFileFormatException 
+	 */
+	public void getDescription(String n){
+		QuestGiverStorage storage = new QuestGiverStorage();
+		try {
+			storage.LoadNPCDescription(n);
+		} catch (InvalidFileFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		name = storage.getName();
+		locX = storage.getLocation().getBlockX();
+		locY = storage.getLocation().getBlockY();
+		locZ = storage.getLocation().getBlockZ();
+		skin = storage.getSkin();
+		cape = storage.getCape();
+		noquestmessage = storage.getNoquestmessage();
+		havequestmessage = storage.getHavequestmessage();
+		quests = storage.getQuests();
+		recommend = storage.getRecommend();
+	}
 	
 	/* Automatically generated getters and setters by Eclipse */
 
 	public String getName() {
-		return name;
+		return name.replaceAll(" ", "_");
 	}
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
 	}
 
 	public Location getLocation() {
@@ -119,11 +174,11 @@ public class QuestGiverManager {
 		this.quests = quests;
 	}
 
-	public long[] getRecommend() {
+	public String[] getRecommend() {
 		return recommend;
 	}
 
-	public void setRecommend(long[] recommend) {
+	public void setRecommend(String[] recommend) {
 		this.recommend = recommend;
 	}
 	
--- src/main/java/com/theminequest/MQCoreNPC/QuestGivers/QuestGiverStorage.java
+++ src/main/java/com/theminequest/MQCoreNPC/QuestGivers/QuestGiverStorage.java
@@ -16,6 +16,8 @@ import org.ini4j.InvalidFileFormatException;
 import org.ini4j.Profile.Section;
 import org.ini4j.spi.IniBuilder;
 
+import com.theminequest.MQCoreNPC.MQCoreNPC;
+
 
 public class QuestGiverStorage {
 	private File file;
@@ -27,18 +29,20 @@ public class QuestGiverStorage {
 	private String noquestmessage;
 	private String havequestmessage;
 	private String[] quests;
-	private long[] recommend;
+	private String[] recommend;
+	private QuestGiverManager qgm;
 	
 	/**
-	 * Load an NPC description object from a file.
+	 * Load an NPC description object from a name.
 	 * @param f
 	 * @throws InvalidFileFormatException
 	 * @throws IOException
 	 */
-	public void LoadNPCDescription(File f) throws InvalidFileFormatException, IOException{
+	public void LoadNPCDescription(String name) throws InvalidFileFormatException, IOException{
 		Ini ini = new Ini();
-		ini.load(f);
-		file = f;
+		file = new File(MQCoreNPC.activePlugin.getDataFolder()+"NPC/"+"General/"+name+".npc");
+		ini.load(file);
+		
 		/*
 		 * General NPC Properties. This includes the name, ID number,
 		 * location of the NPC, and messages it tells the user.
@@ -75,14 +79,58 @@ public class QuestGiverStorage {
 		 * go to for quests? (by ID number)
 		 */
 		Ini.Section recommendsection = ini.get("Recommendations");
-		recommend = recommendsection.getAll("recommend", long[].class);
+		recommend = recommendsection.getAll("recommend", String[].class);
 	}
 	
+	public void LoadNPCDescription(File file) throws InvalidFileFormatException, IOException{
+		Ini ini = new Ini();
+		file = new File(file.getAbsolutePath());
+		ini.load(file);
+		
+		/*
+		 * General NPC Properties. This includes the name, ID number,
+		 * location of the NPC, and messages it tells the user.
+		 * In addition, if you have Spoutcraft, you can give the NPC
+		 * a skin and cape.
+		 */
+		Ini.Section properties = ini.get("Properties");
+		name = properties.get("name");
+		id = Long.parseLong(properties.get("id"));
+		String world = properties.get("world");
+		double locationX = Double.parseDouble(properties.get("locX"));
+		double locationY = Double.parseDouble(properties.get("locY"));
+		double locationZ = Double.parseDouble(properties.get("locZ"));
+		location = new Location(Bukkit.getWorld(world),locationX,locationY,locationZ);
+		if (properties.containsKey("skin")) skin = properties.get("skin");
+		else skin = "http://www.minecraft.net/images/char.png";
+		if (properties.containsKey("cape")) cape = properties.get("cape");
+		else cape = "";
+		if (properties.containsKey("noquest")) noquestmessage = properties.get("noquest");
+		else noquestmessage = "Sorry, I don't have anything for ya.";
+		if (properties.containsKey("havequest")) havequestmessage = properties.get("havequest");
+		else havequestmessage = "Let's get started.";
+		
+		/*
+		 * Quests - What quests does this NPC make available?
+		 */
+		Ini.Section questsection = ini.get("Quests");
+		quests = questsection.getAll("give", String[].class);
+		
+		/*
+		 * Recommendations - When all quests are completed (all
+		 * are non-repeating and this NPC has no more to give),
+		 * which other NPCs do you want to recommend the player
+		 * go to for quests? (by ID number)
+		 */
+		Ini.Section recommendsection = ini.get("Recommendations");
+		recommend = recommendsection.getAll("recommend", String[].class);
+	}
 	/**
 	 * Save the NPC description to a file.
 	 * @throws IOException if unable to save.
 	 */
-	private void save() throws IOException{
+	public void save(String name, Location location, String skin, String cape, String noquestmessage, 
+			String havequestmessage, String[] quests, String[] recommend) throws IOException{
 		Ini ini = new Ini();
 		Ini.Section properties = ini.add("Properties");
 		properties.put("name", name);
@@ -100,5 +148,76 @@ public class QuestGiverStorage {
 		Ini.Section recommendsection = ini.get("Recommendations");
 		recommendsection.add("recommend",recommend);
 		ini.store(file);
+	}
+	
+	public String getFile(int n){
+		
+		return name;
+	}
+	
+	//Getters  and setters. 
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public String getSkin() {
+		return skin;
+	}
+
+	public void setSkin(String skin) {
+		this.skin = skin;
+	}
+
+	public String getCape() {
+		return cape;
+	}
+
+	public void setCape(String cape) {
+		this.cape = cape;
+	}
+
+	public String getNoquestmessage() {
+		return noquestmessage;
+	}
+
+	public void setNoquestmessage(String noquestmessage) {
+		this.noquestmessage = noquestmessage;
+	}
+
+	public String getHavequestmessage() {
+		return havequestmessage;
+	}
+
+	public void setHavequestmessage(String havequestmessage) {
+		this.havequestmessage = havequestmessage;
+	}
+
+	public String[] getQuests() {
+		return quests;
+	}
+
+	public void setQuests(String[] quests) {
+		this.quests = quests;
+	}
+
+	public String[] getRecommend() {
+		return recommend;
+	}
+
+	public void setRecommend(String[] recommend) {
+		this.recommend = recommend;
 	}
 }
